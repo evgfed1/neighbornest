53713ee7f14dee5bce27524c8bb2fd16cded0b73
Evgeny <epsilon1293@gmail.com>
2023-12-18 19:24:35 +0200

    Initial first commit

---
+++ src/main/java/com/antevgproject/neighbornest/NeighbornestApplication.java
@@ -0,0 +1,13 @@
+package com.antevgproject.neighbornest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighbornestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighbornestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antevgproject/neighbornest/NeighbornestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antevgproject.neighbornest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbornestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
