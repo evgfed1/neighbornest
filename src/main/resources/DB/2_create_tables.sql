36daf4fb3aecffef1abb927de4d1830fb721914a
Evgeny <epsilon1293@gmail.com>
2023-12-21 14:58:57 +0200

    1_reset_database.sql  ISPRAVITJ 2_create_tables.sql DONE 3_import_data.sql DONE
    docker-compose done

---
+++ src/main/resources/DB/2_create_tables.sql
@@ -0,0 +1,174 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2023-12-20 16:58:25.275
+
+-- tables
+-- Table: apartment
+CREATE TABLE apartment (
+                           id serial  NOT NULL,
+                           building_id int  NOT NULL,
+                           number int  NOT NULL,
+                           floor int  NOT NULL,
+                           number_of_rooms int  NOT NULL,
+                           area int  NOT NULL,
+                           balcony boolean  NOT NULL,
+                           CONSTRAINT apartment_pk PRIMARY KEY (id)
+);
+
+-- Table: association
+CREATE TABLE association (
+                             id serial  NOT NULL,
+                             building_id int  NOT NULL,
+                             name varchar(255)  NOT NULL,
+                             address varchar(255)  NOT NULL,
+                             postindex varchar(255)  NOT NULL,
+                             phone varchar(255)  NOT NULL,
+                             email varchar(255)  NOT NULL,
+                             regnumber varchar(255)  NOT NULL,
+                             CONSTRAINT id PRIMARY KEY (id)
+);
+
+-- Table: building
+CREATE TABLE building (
+                          id serial  NOT NULL,
+                          date_of_build date  NOT NULL,
+                          cadastral varchar(255)  NOT NULL,
+                          floors int  NOT NULL,
+                          lift boolean  NOT NULL,
+                          CONSTRAINT building_pk PRIMARY KEY (id)
+);
+
+-- Table: consumption
+CREATE TABLE consumption (
+                             id serial  NOT NULL,
+                             created_by_resident_id int  NOT NULL,
+                             apartment_id int  NOT NULL,
+                             hot_water decimal(8,3)  NULL,
+                             cold_water decimal(8,3)  NULL,
+                             electricity decimal(8,3)  NULL,
+                             gas decimal(8,3)  NULL,
+                             created_at date  NOT NULL,
+                             CONSTRAINT consumption_pk PRIMARY KEY (id)
+);
+
+-- Table: lift
+CREATE TABLE lift (
+                      id serial  NOT NULL,
+                      building_id int  NOT NULL,
+                      current_inspection date  NOT NULL,
+                      next_inspection date  NOT NULL,
+                      service_company varchar(255)  NOT NULL,
+                      CONSTRAINT lift_pk PRIMARY KEY (id)
+);
+
+-- Table: profile
+CREATE TABLE profile (
+                         id serial  NOT NULL,
+                         role_id int  NOT NULL,
+                         username varchar(255)  NOT NULL,
+                         password varchar(255)  NOT NULL,
+                         CONSTRAINT profile_pk PRIMARY KEY (id)
+);
+
+-- Table: resident
+CREATE TABLE resident (
+                          id serial  NOT NULL,
+                          profile_id int  NOT NULL,
+                          first_name varchar(255)  NOT NULL,
+                          last_name varchar(255)  NOT NULL,
+                          email varchar(255)  NOT NULL,
+                          telefon varchar(255)  NOT NULL,
+                          birthdate date  NOT NULL,
+                          status char(1)  NOT NULL,
+                          CONSTRAINT resident_pk PRIMARY KEY (id)
+);
+
+-- Table: resident_apartment
+CREATE TABLE resident_apartment (
+                                    id serial  NOT NULL,
+                                    apartment_id int  NOT NULL,
+                                    resident_id int  NOT NULL,
+                                    CONSTRAINT resident_apartment_pk PRIMARY KEY (id)
+);
+
+-- Table: role
+CREATE TABLE role (
+                      id serial  NOT NULL,
+                      name varchar(255)  NOT NULL,
+                      CONSTRAINT role_pk PRIMARY KEY (id)
+);
+
+-- foreign keys
+-- Reference: apartment_building (table: apartment)
+ALTER TABLE apartment ADD CONSTRAINT apartment_building
+    FOREIGN KEY (building_id)
+        REFERENCES building (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: association_building (table: association)
+ALTER TABLE association ADD CONSTRAINT association_building
+    FOREIGN KEY (building_id)
+        REFERENCES building (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: consumption_apartment (table: consumption)
+ALTER TABLE consumption ADD CONSTRAINT consumption_apartment
+    FOREIGN KEY (apartment_id)
+        REFERENCES apartment (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: consumption_resident (table: consumption)
+ALTER TABLE consumption ADD CONSTRAINT consumption_resident
+    FOREIGN KEY (created_by_resident_id)
+        REFERENCES resident (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: lift_building (table: lift)
+ALTER TABLE lift ADD CONSTRAINT lift_building
+    FOREIGN KEY (building_id)
+        REFERENCES building (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: profile_role (table: profile)
+ALTER TABLE profile ADD CONSTRAINT profile_role
+    FOREIGN KEY (role_id)
+        REFERENCES role (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: resident_apartment_apartment (table: resident_apartment)
+ALTER TABLE resident_apartment ADD CONSTRAINT resident_apartment_apartment
+    FOREIGN KEY (apartment_id)
+        REFERENCES apartment (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: resident_apartment_resident (table: resident_apartment)
+ALTER TABLE resident_apartment ADD CONSTRAINT resident_apartment_resident
+    FOREIGN KEY (resident_id)
+        REFERENCES resident (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: resident_profile (table: resident)
+ALTER TABLE resident ADD CONSTRAINT resident_profile
+    FOREIGN KEY (profile_id)
+        REFERENCES profile (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
